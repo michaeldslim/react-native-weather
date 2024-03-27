87ff3008171aab0975a1f708624e119af5d15c5f
Mike Lim <dongseok.lim@gmail.com>
2024-03-27 13:44:07 -0500

    Test commit

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ const icons = {
 };
 
 export default function App() {
-  const [city, setCity] = useState("Loading...");
+  const [city, setCity] = useState("Loading.....");
   const [days, setDays] = useState([]);
   const [, setOk] = useState(true);
   const getWeather = async () => {
